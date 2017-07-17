232b4a38931e698d60080dee2b017811c0ae138f
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-17 09:35:58 -0400

    Topic model

--- goodblogger/models.py
+++ goodblogger/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    """A topic that a blog post can have"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """How to represent an instance as a string"""
+        return self.text
