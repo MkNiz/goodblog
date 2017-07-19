a97baa76286936781722fce411b2f2b525d32713
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-19 10:13:12 -0400

    Topics url, view, template

--- goodblogger/urls.py
+++ goodblogger/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     # Home
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+
+    # Topics
+    url(r'^topics/$', views.topics, name='topics')
 ]
--- goodblogger/views.py
+++ goodblogger/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     return render(request, 'goodblogger/index.html')
+
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.order_by('date_added')
+    return render(request, 'goodblogger/topics.html', {'topics': topics})
