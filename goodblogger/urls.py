db820a8af10b0546d2c52fd60f0a65c319304da9
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-19 10:24:55 -0400

    Topic url, view, template

--- goodblogger/urls.py
+++ goodblogger/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     # Topics
-    url(r'^topics/$', views.topics, name='topics')
+    url(r'^topics/$', views.topics, name='topics'),
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
 ]
--- goodblogger/views.py
+++ goodblogger/views.py
@@ -9,3 +9,10 @@ def topics(request):
     """Show all topics"""
     topics = Topic.objects.order_by('date_added')
     return render(request, 'goodblogger/topics.html', {'topics': topics})
+
+def topic(request, topic_id):
+    """Displays a single topic and its posts"""
+    topic = Topic.objects.get(id=topic_id)
+    posts = topic.post_set.order_by('-date_added')
+    context = {'topic': topic, 'posts': posts}
+    return render(request, 'goodblogger/topic.html', context) 
