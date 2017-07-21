f6d5793033f50db43ec0a3aa60c3bd87e85c96ed
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-21 09:51:45 -0400

    User login url and template

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+"""Defines URL patterns for the usersystem"""
+
+from django.conf.urls import url
+from django.contrib.auth.views import login
+from . import views
+
+urlpatterns = [
+    # Login Page
+    url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
+]
