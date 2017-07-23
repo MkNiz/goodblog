ffcbef3ba952d295f449baef5f573092c083ce03
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-23 09:39:59 -0400

    Topics owned by users

--- goodblogger/models.py
+++ goodblogger/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
     """A topic that a blog post can have"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User)
 
     def __str__(self):
         """How to represent an instance as a string"""
