0752ffc45ca5196bae45423d8195d5b0d7ae5b04
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-23 10:03:41 -0400

    Current user set as owner of a new topic

--- goodblogger/views.py
+++ goodblogger/views.py
@@ -37,7 +37,9 @@ def new_topic(request):
         # Utilize POST data
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('blog:topics'))
     return render(request, 'goodblogger/new_topic.html', {'form': form})
 
