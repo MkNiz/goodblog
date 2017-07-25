e692a66283928ce973c8fc3499de51ce4a3f227c
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-25 11:49:40 -0400

    DEBUG false when deployed

--- goodblog/settings.py
+++ goodblog/settings.py
@@ -146,6 +146,8 @@ if os.getcwd() == '/app':
     # Allow all host headers
     ALLOWED_HOSTS = ['*']
 
+    DEBUG = False
+
     # Static asset config
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
